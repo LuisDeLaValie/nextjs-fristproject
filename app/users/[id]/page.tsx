71eeb60b3588013e8a1a2734272db6ffbf5197ae
TDTxLE <TDTxLE@gmail.com>
2024-04-07 00:33:14 -0600

    dar estilo  ala vista usario

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -15,12 +15,11 @@ type User = {
 
 export default async function UserPage({ params }: any) {
   const user = await fetchUser(params.id);
-  console.log(params);
 
   return (
-    <div>
-      <img src={user.avatar} />
-      <h3> {user.first_name} {user.last_name} </h3>
+    <div className="bg-slate-400 p-10 rounded-md">
+      <img src={user.avatar}  className="m-auto my-4"/>
+      <h3 className="text-3xl font-bold"> {user.first_name} {user.last_name} </h3>
       <p> {user.email} </p>
     </div>
   );
