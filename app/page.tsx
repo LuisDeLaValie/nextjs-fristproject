708c75b29ff189ceb5802932f2fdd5eba90f34d8
TDTxLE <TDTxLE@gmail.com>
2024-04-07 00:07:20 -0600

    agregar estilos

--- app/page.tsx
+++ app/page.tsx
@@ -18,13 +18,17 @@ export default async function HomePage() {
   return (
     <ul>
       {usuarios.map((user) => (
-        <li key={user.id}>
+        <li
+          key={user.id}
+          className="bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between"
+        >
           <div>
-            <h5>
+            <h5 className="font-bold">
               {user.id} {user.first_name} {user.last_name}
             </h5>
-            <p>{user.email}</p>
+            <p className="text-slate-100">{user.email}</p>
           </div>
+          <img src={user.avatar} alt="avatar" className="w-20 rounded-full" />
         </li>
       ))}
     </ul>
